fbf680ec410f915bc435a726491d40cdcb3f2149
Peter Borkuti <peter@aspire>
2016-05-19 22:57:06 +0200

    01-pubsub default app - initial commit

---
+++ 01-pubsub/app/src/androidTest/java/com/ros/android/pubsub/borkuti/peter/ex01pubsub/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.ros.android.pubsub.borkuti.peter.ex01pubsub;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ 01-pubsub/app/src/test/java/com/ros/android/pubsub/borkuti/peter/ex01pubsub/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.ros.android.pubsub.borkuti.peter.ex01pubsub;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
